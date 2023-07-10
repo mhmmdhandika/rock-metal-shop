3478631e1282955b2075ff2202a6ba176fb11666
mhmmdhandika <muhammadhandika78@gmail.com>
2023-07-10 21:10:01 +0700

    create wishlist page and endpoint (complete)

--- src/components/ShoppingProductList/ActionButtons.jsx
+++ src/components/ShoppingProductList/ActionButtons.jsx
@@ -6,8 +6,15 @@ import {
 } from "react-icons/ai";
 import { MdOutlineFavoriteBorder as FavoriteIcon } from "react-icons/md";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
-export default function ActionButton({ itemId }) {
+export default function ActionButton({ userId, itemId }) {
+  const { data: session } = useSession()
+
+  const handleWishlist = (userId, itemId) => {
+    console.log(session)
+  }
+
   return (
     <div className="flex w-full items-center justify-center gap-3">
       <button className="ease rounded-full bg-white p-2 transition-all duration-300 hover:scale-125">
@@ -19,7 +26,10 @@ export default function ActionButton({ itemId }) {
       >
         <SearchIcon size={20} />
       </Link>
-      <button className="ease rounded-full bg-white p-2 transition-all duration-300 hover:scale-125">
+      <button 
+        className="ease rounded-full bg-white p-2 transition-all duration-300 hover:scale-125"
+        onClick={() => handleWishlist(userId, itemId)}
+      >
         <FavoriteIcon size={20} />
       </button>
     </div>
