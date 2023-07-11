80e7737c83219298e20516668904ab9db03ebae8
mhmmdhandika <muhammadhandika78@gmail.com>
2023-07-11 09:12:41 +0700

    change wishlist page design

--- src/app/(main)/wishlists/page.jsx
+++ src/app/(main)/wishlists/page.jsx
@@ -4,8 +4,11 @@ import { userRequest } from "@/axios/requestMethods";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useState, useEffect } from "react";
+import { AiFillHeart as FillHeartIcon } from "react-icons/ai";
 import { AiOutlineShoppingCart as CartIcon } from "react-icons/ai";
-import { BsTrash3 as TrashIcon } from "react-icons/bs";
+import { BsTrash3 as TrashIcon } from "react-icons/bs"
+import ActionButtons from "@/components/ShoppingProductList/ActionButtons";
+
 
 export default function WishlistsPage() {
   const { data: session } = useSession();
@@ -14,53 +17,39 @@ export default function WishlistsPage() {
   useEffect(() => {
     if (session) {
       const fetchWislistProducts = async () => {
-      const response = await userRequest(session?.user.accessToken).get("/api/wishlist");
-      setProducts(response.data.products);
+        const response = await userRequest(session?.user.accessToken).get(
+          "/api/wishlist"
+        );
+        setProducts(response.data.products);
       };
       fetchWislistProducts();
-      console.log(products)
-      }
+      console.log(products);
+    }
   }, [session]);
 
   return (
     <div>
       <div className="section-space-x">
         {/* card products container */}
-        <div className="mx-auto grid max-w-[25rem] items-center gap-5 md:mx-0 md:max-w-none md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="mx-auto grid max-w-[25rem] items-center gap-2 md:mx-0 md:max-w-none md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {/* card product */}
           {products.length > 1 ? (
-            products.map((item) => (
-              <div>
-                <div>
-                  <img src={item.product.img} alt={item.product.title} />
-                </div>
-                <div className="grid grid-cols-[auto_20%] gap-2">
-                  <div className="flex flex-col gap-4">
-                    <Link
-                      href={`/product/${item.product._id}`}
-                      className="block max-w-[15rem] truncate text-3xl font-bold hover:underline"
-                    >
+            products.map((item, index) => (
+              <div
+                key={index}
+                className="group relative flex flex-col justify-center overflow-hidden bg-slate-100"
+              >
+                <img
+                  src={item.product.img}
+                  alt={item.product.title}
+                  className="w-full px-5"
+                />
+                <div className="duration-800 ease absolute z-10 h-full w-full bg-black/30  opacity-0 transition-all group-hover:opacity-100">
+                  <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                    <h1 className="mb-2 text-center text-lg font-semibold text-white">
                       {item.product.title}
-                    </Link>
-                    <div className="flex gap-2">
-                      {item.product?.color.map((color) => (
-                        <div
-                          className="h-7 w-7 rounded-full"
-                          style={{ backgroundColor: color }}
-                        ></div>
-                      ))}
-                    </div>
-                  </div>
-                  <div className="flex flex-col items-end gap-4">
-                    <h3 className="text-3xl">${item.product.price}</h3>
-                    <div className="flex gap-2">
-                      <button className="rounded-md border-2 border-black p-2">
-                        <CartIcon size={18} />
-                      </button>
-                      <button className="rounded-md border-2 border-black p-2">
-                        <TrashIcon size={18} />
-                      </button>
-                    </div>
+                    </h1>
+                    <ActionButtons itemId={item.id} />
                   </div>
                 </div>
               </div>
