94ab4074323ae7c199fae387403e3329ebb04a0b
mhmmdhandika <muhammadhandika78@gmail.com>
2023-07-12 19:13:12 +0700

    add wishlist reduxx

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "../actions/cartSlice.js";
+import wishlistSlice from "../actions/wishlistSlice.js";
 
 export default configureStore({
   reducer: {
     cart: cartSlice,
+    wishlist: wishlistSlice,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
