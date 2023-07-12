6444ebf3ceb92f83d24bbad16a7569c7c5a6549d
mhmmdhandika <muhammadhandika78@gmail.com>
2023-07-12 20:39:33 +0700

    add fetchWishlist and fixed status conditional in get cart items useEffect in file

--- src/components/FirstUserRequest.jsx
+++ src/components/FirstUserRequest.jsx
@@ -4,26 +4,42 @@ import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getAllCartItem } from "@/redux/actions/cartSlice";
 import { useSession } from "next-auth/react";
+import { getAllWishlist } from "@/redux/actions/wishlistSlice";
 
 export default function FirstUserRequest({ children }) {
   const dispatch = useDispatch();
 
   const { data: session, status } = useSession();
 
+  // get cart items
   useEffect(() => {
-    const fetchCartItems = () => {
-      if (status === 'authenticated') {
+    if (status === "authenticated") {
+      const fetchCartItems = () => {
         dispatch(getAllCartItem({ token: session?.user.accessToken }));
-      }
+      };
+      // fetch cart items immediately when the component mounts
+      fetchCartItems();
+
+      // fetch cart items every 60 seconds
+      const intervalId = setInterval(fetchCartItems, 60 * 1000);
+
+      // clean up the interval on component unmount
+      return () => clearInterval(intervalId);
     }
-    // fetch cart items immediately when the component mounts
-    fetchCartItems();
+  }, [status]);
 
-    // fetch cart items every 10 seconds
-    const intervalId = setInterval(fetchCartItems, 60 * 1000)
+  // get wishlist
+  useEffect(() => {
+    if (status === "authenticated") {
+      const fetchWishlist = () => {
+        dispatch(getAllWishlist({ token: session?.user.accessToken }));
+      };
+      fetchWishlist();
 
-    // clean up the interval on component unmount
-    return () => clearInterval(intervalId)
+      const intervalId = setInterval(fetchWishlist, 60 * 1000);
+
+      return () => clearInterval(intervalId);
+    }
   }, [status]);
 
   return children;
