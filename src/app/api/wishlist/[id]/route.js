92b8f01fd62871ae90f4197a3c20b10009aa42cc
mhmmdhandika <muhammadhandika78@gmail.com>
2023-07-13 08:54:37 +0700

    create DELETE wishlist api

---
+++ src/app/api/wishlist/[id]/route.js
@@ -0,0 +1,38 @@
+import { NextResponse } from 'next/server';
+import Product from '@/models/Product'
+import Wishlist from '@/models/Wishlist';
+import connectToMongoDB from '@/lib/mongodb';
+import { verifyToken } from '@/helpers/verifyToken';
+import VerifyTokenError from '@/errors/VerifyTokenError';
+
+// DELETE A WISHLIST
+export async function DELETE(request, context) {
+  try {
+    await connectToMongoDB()
+
+    const token = request.headers.get('token');
+    const productId = context.params.id;
+    const userTokenData = await verifyToken(token);
+
+    if (userTokenData.error) {
+      throw new VerifyTokenError(userTokenData.message, {
+        status: userTokenData.status
+      }) 
+    }
+
+    const userWishlist = await Wishlist.findOneAndUpdate(
+      { userId: userTokenData.id },
+      { $pull: { products: { product: productId } } },
+      { new: true }
+    )
+
+    return NextResponse.json({
+      data: userWishlist
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: error.message },
+      { status: error?.data?.status ?? 500 }
+    ); 
+  }
+}
