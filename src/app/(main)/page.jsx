66d1e9c73ba5f67e0fd72f322cf5728936f345f0
mhmmdhandika <muhammadhandika78@gmail.com>
2023-07-14 21:28:29 +0700

    create side nav (not unauthenticated yet)

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -2,6 +2,8 @@ import { Suspense } from "react";
 import Header from "@/partials/home/Header";
 import ShoppingProductListServer from "@/components/ShoppingProductList/ServerComponent";
 import Loading from "@/components/Loading";
+import Link from "next/link";
+import { AiOutlineArrowRight as ArrowRight } from 'react-icons/ai'
 
 export default function Home() {
   return (
@@ -9,7 +11,11 @@ export default function Home() {
       <Suspense fallback={<Loading />}>
         <Header />
       </Suspense>
-        <Suspense fallback={<Loading />}>
+        <Suspense fallback={<Loading />}> 
+        <div className="section-space-x mt-5 flex justify-between items-center">
+          <h2 className="text-xl">Featured products</h2>
+          <Link href="/products" className="py-1 px-2 text-lg flex items-center gap-2 hover:underline">Show all <ArrowRight size={15}/></Link>
+        </div>
         <ShoppingProductListServer
           limit={12}
         />
