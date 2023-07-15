cc5f456fe0830baa8e7f89f04a1c2ec21d7e4262
mhmmdhandika <muhammadhandika78@gmail.com>
2023-07-15 14:20:41 +0700

    change font to snorkad

---
+++ public/assets/fonts/revolver/readme.txt
@@ -0,0 +1,7 @@
+This work is licensed under the Creative Commons Attribution-NoDerivatives 4.0 International (CC BY-ND 4.0)
+
+Any change on the font file is prohibited.
+You cant sell or lease this font.
+You can use it in both, commercial and personal projects and you can share it with everyone, you dont have to attribute Eduardo Aire Torres on your designs or social networks but if you do it, it could be great.
+
+https://eduairet.com
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,15 @@
 import NextAuthProvider from "@/components/NextAuthProvider";
 import "@/style/globals.css";
 import { Lato } from "next/font/google";
+import localFont from "next/font/local";
 import ReduxClientProvider from "@/components/ReduxClientProvider";
 import FirstUserRequest from "@/components/FirstUserRequest";
+
+const snorkad = localFont({
+  src: "../../public/assets/fonts/snorkad/snorkad-regular.otf",
+  display: "fallback",
+  fallback: ["system-ui", "arial"],
+});
 
 const lato = Lato({
   subsets: ["latin"],
@@ -18,7 +25,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={lato.className} suppressHydrationWarning={true}>
+      <body className={snorkad.className} suppressHydrationWarning={true}>
         <NextAuthProvider>
           <ReduxClientProvider>
             <FirstUserRequest>{children}</FirstUserRequest>
