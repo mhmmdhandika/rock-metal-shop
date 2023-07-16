1d9036c79c661795e5f1523d6c3859a90ab3de3d
mhmmdhandika <muhammadhandika78@gmail.com>
2023-07-16 09:39:05 +0700

    re-change style

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 
-import loginUser from '@/helpers/loginUser';
-import Link from 'next/link';
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import Swal from 'sweetalert2';
-import { useRouter } from 'next/navigation';
+import loginUser from "@/helpers/loginUser";
+import Link from "next/link";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import { useRouter } from "next/navigation";
 
 function Register() {
   const router = useRouter();
@@ -15,15 +15,15 @@ function Register() {
 
   const { ORIGIN_URL } = process.env;
 
-  const onSubmit = async data => {
+  const onSubmit = async (data) => {
     setIsLoading(true);
     // exclude confirmPassword
     const { confirmPassword, ...otherUserData } = data;
 
-    const res = await fetch('/api/auth/register', {
-      method: 'POST',
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(otherUserData),
     });
@@ -34,8 +34,8 @@ function Register() {
     if (data.password !== data.confirmPassword) {
       setIsLoading(false);
       return Swal.fire({
-        title: 'Invalid Confirm Password',
-        icon: 'error',
+        title: "Invalid Confirm Password",
+        icon: "error",
       });
     }
 
@@ -46,10 +46,10 @@ function Register() {
       });
 
       if (!resLogin && !resLogin.ok) {
-        return console.log('error resLogin');
+        return console.log("error resLogin");
       }
 
-      router.push('/');
+      router.push("/");
     }
 
     // is username exists
@@ -58,82 +58,82 @@ function Register() {
   };
 
   return (
-    <div 
-      className='relative min-h-screen w-full object-center'
+    <div
+      className="relative min-h-screen w-full object-center"
       style={{
         backgroundImage: `url("/assets/img/auth/bg-register.jpg"), linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))`,
         backgroundPosition: "center",
         backgroundSize: "cover",
         backgroundBlendMode: "multiply",
-        backgroundAttachment: 'fixed',
+        backgroundAttachment: "fixed",
       }}
     >
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className='form-auth-register absolute left-1/2 top-1/2 flex min-w-[25rem] -translate-x-1/2 -translate-y-1/2 flex-col gap-5 bg-black/60 p-7 text-stone-400 border-2 border-stone-400'
+        className="form-auth-register absolute left-1/2 top-1/2 flex min-w-[25rem] -translate-x-1/2 -translate-y-1/2 flex-col gap-5 border-2 border-white bg-black/50 p-7 text-white"
       >
-        <h1 className='text-3xl font-light'>CREATE AN ACCOUNT</h1>
-        <div className='grid md:grid-cols-2 gap-3'>
+        <h1 className="text-3xl font-light">CREATE AN ACCOUNT</h1>
+        <div className="grid gap-3 md:grid-cols-2">
           {/* first name */}
           <input
-            type='text'
-            placeholder='first name'
-            {...register('firstName', { required: true })}
-            className="bg-transparent border-stone-400"
+            type="text"
+            placeholder="first name"
+            {...register("firstName", { required: true })}
+            className="border-white bg-transparent"
           />
           {/* last name */}
           <input
-            type='text'
-            placeholder='last name'
-            {...register('lastName')}
-            className="bg-transparent border-stone-400"
+            type="text"
+            placeholder="last name"
+            {...register("lastName")}
+            className="border-white bg-transparent"
           />
           {/* username */}
           <input
-            type='text'
-            placeholder='username'
-            {...register('username', { required: true })}
-            className="bg-transparent border-stone-400"
+            type="text"
+            placeholder="username"
+            {...register("username", { required: true })}
+            className="border-white bg-transparent"
           />
           {/* email */}
           <input
-            type='email'
-            placeholder='email'
-            {...register('email', { required: true })}
-            className="bg-transparent border-stone-400"
+            type="email"
+            placeholder="email"
+            {...register("email", { required: true })}
+            className="border-white bg-transparent"
           />
           {/* password */}
           <input
-            type='password'
-            placeholder='password'
-            {...register('password', { required: true })}
-            className="bg-transparent border-stone-400"
+            type="password"
+            placeholder="password"
+            {...register("password", { required: true })}
+            className="border-white bg-transparent"
           />
           {/* confirm password */}
           <input
-            type='password'
-            placeholder='confirm password'
-            {...register('confirmPassword', { required: true })}
-            className="bg-transparent border-stone-400d"
+            type="password"
+            placeholder="confirm password"
+            {...register("confirmPassword", { required: true })}
+            className="border-whited bg-transparent"
           />
         </div>
-        <p className='text-sm'>
+        <p className="text-sm">
           By creating an account or content to the processing of my personal
           data in accordance with the <b>PRIVACY POLICY</b>
         </p>
-        <div className='flex justify-end gap-3'>
+        <div className="flex justify-end gap-3">
           <Link
-            href='/'
-            className='text-md bg-transparent px-5 py-2 font-semibold text-stone-400 border-2 border-stone-400'
+            href="/"
+            className="text-md border-2 border-white bg-transparent px-5 py-2 font-semibold text-white"
           >
             BACK
           </Link>
           <button
             disabled={isLoading}
-            type='submit'
-            className='text-md bg-slate-900 border-2 border-stone-400 px-5 py-2 font-semibold text-white'
+            type="submit"
+            className="text-md border-2 border-white bg-slate-900 px-5 py-2 font-semibold text-white"
           >
-            {isLoading ? 'Loading...' : 'SUBMIT'}
+            {isLoading ? "Loading..." : "SUBMIT"}
           </button>
         </div>
       </form>
