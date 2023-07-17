2f974979cc126a54443b5dd2fba76e6b2a2b7667
mhmmdhandika <muhammadhandika78@gmail.com>
2023-07-17 21:26:06 +0700

    filter background header to black and white

--- src/partials/home/Header.jsx
+++ src/partials/home/Header.jsx
@@ -30,6 +30,7 @@ function Header() {
                 backgroundPosition: "center",
                 backgroundSize: "cover",
                 backgroundBlendMode: "multiply",
+                filter: "grayscale(1)"
               }}
             >
               <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-2 text-center text-white">
