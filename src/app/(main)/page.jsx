44be4e0eda2d08d08af84ac500bbc61fce76033b
mhmmdhandika <muhammadhandika78@gmail.com>
2023-07-17 11:09:58 +0700

    change shopping product list loading on ShoppingProductListServer

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import Header from "@/partials/home/Header";
 import ShoppingProductListServer from "@/components/ShoppingProductList/ServerComponent";
+import ShoppingProductListLoading from "@/components/ShoppingProductList/Loading";
 import Loading from "@/components/Loading";
 import Link from "next/link";
 import { AiOutlineArrowRight as ArrowRight } from 'react-icons/ai'
@@ -11,7 +12,7 @@ export default function Home() {
       <Suspense fallback={<Loading />}>
         <Header />
       </Suspense>
-        <Suspense fallback={<Loading />}> 
+        <Suspense fallback={<ShoppingProductListLoading />}> 
         <div className="section-space-x mt-5 flex justify-between items-center">
           <h2 className="text-xl">Featured products</h2>
           <Link href="/products" className="py-1 px-2 text-lg flex items-center gap-2 hover:underline">Show all <ArrowRight size={15}/></Link>
