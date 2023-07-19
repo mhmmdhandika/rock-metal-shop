f1643a8f0220da486015b8f223ea8453d5c30859
mhmmdhandika <muhammadhandika78@gmail.com>
2023-07-19 20:35:02 +0700

    rechange bg header back to un-grayscale

--- src/partials/home/Header.jsx
+++ src/partials/home/Header.jsx
@@ -26,11 +26,10 @@ function Header() {
             <div
               className="relative min-h-screen w-full"
               style={{
-                backgroundImage: `url("${item.img}"), linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))`,
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("${item.img}")`,
                 backgroundPosition: "center",
                 backgroundSize: "cover",
                 backgroundBlendMode: "multiply",
-                filter: "grayscale(1)"
               }}
             >
               <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-2 text-center text-white">
