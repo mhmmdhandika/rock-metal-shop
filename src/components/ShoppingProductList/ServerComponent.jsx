7b1ea33164a5927eb8c556c6778d6ad89d5f09e1
mhmmdhandika <muhammadhandika78@gmail.com>
2023-07-23 09:33:54 +0700

    fix error products.map

--- src/components/ShoppingProductList/ServerComponent.jsx
+++ src/components/ShoppingProductList/ServerComponent.jsx
@@ -27,7 +27,7 @@ export default async function ShoppingProductList(props) {
   return (
     <div className="section-space-x my-5">
       <div className="grid grid-cols-1 justify-between gap-2 sm:grid-cols-2 lg:grid-cols-4">
-        {products?.map((item, index) => (
+        {Array.isArray(products) && products?.map((item, index) => (
           <Product item={item} key={index} />
         ))}
       </div>
